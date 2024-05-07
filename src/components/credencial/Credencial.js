8babe770fdaeba9280ea672245deeb8d7340dcfd
Regulitos <garciajuandavid1999@gmail.com>
2024-05-07 16:54:54 -0500

    agregndo

--- src/components/credencial/Credencial.js
+++ src/components/credencial/Credencial.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import user from '../userLoggedIn.json';
 import './credencial.css';
 import { Link } from 'react-router-dom';
 import LogoutIcon from '@mui/icons-material/Logout';
-
+import Swal from "sweetalert2";
 
 function Credencial() {
     const ultimaEntrada = user[user.length - 1];
     const nombre = ultimaEntrada.nombres;
     const apellido = ultimaEntrada.apellidos;
     const email = ultimaEntrada.email;
+
+    const [autoLogout, setAutoLogout] = useState(false);
+    let executed = false;
+    let timeoutId;
+    useEffect(() => {
+        
+
+        const resetTimeout = () => {
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+
+            timeoutId = setTimeout(() => {
+                setAutoLogout(true);
+            }, 10000);
+        };
+
+        const handleInteraction = () => {
+            if (autoLogout) {
+                setAutoLogout(false);
+            }
+            resetTimeout();
+        };
+
+        resetTimeout();
+
+        window.addEventListener('mousemove', handleInteraction);
+        window.addEventListener('keydown', handleInteraction);
+
+        return () => {
+            window.removeEventListener('mousemove', handleInteraction);
+            window.removeEventListener('keydown', handleInteraction);
+            clearTimeout(timeoutId);
+        };
+    }, [autoLogout]);
+
+    
+    useEffect(() => {
+        if(autoLogout && !executed) {
+            alert("La sesion se cerro por inactividad.");
+        }
+        if (autoLogout) {            
+            window.location.href = '/'; 
+        }
+    }, [autoLogout]);
 
     return (
         <div className="container">
@@ -37,3 +82,4 @@ function Credencial() {
 }
 
 export default Credencial;
+
